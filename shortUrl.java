dcfea6068450b7fecb6974cfc41fdc9e62beeeba
Iverson Ruffin <76574672+iruffin@users.noreply.github.com>
2021-08-16 01:31:15 -0400

    Add files via upload

---
+++ shortUrl.java
@@ -0,0 +1,84 @@
+/*
+ *Author@ Iverson Ruffin
+ *The shortUrl class shortens the size of a url into a random 6 letter alphanumeric string, and can restore the shortened URL to its original (unless there is no such URL, then it will return null)
+ *
+ */
+import java.util.Random;
+
+public class shortUrl {
+   ////Declaring all variables////
+   
+   public String url;
+   public String shortUrl;
+   public String lowerCase = "abcdefghiklmnopqrstuvwxyz";
+   public String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   public String numbers = "0123456789";
+   public String alphaNum = numbers + upperCase + lowerCase;
+   
+   public char[] alphaNumArray = alphaNum.toCharArray();
+   public char[] array = new char[6];
+   
+   ////Constructors////
+   
+   Random r = new Random();
+   
+   //Getters
+   
+   //Returns the value of URL
+   public String getUrl () {
+      return url;
+   } 
+   
+   //Returns the value of short URL
+   public String getShortUrl () {
+      return shortUrl;
+   }  
+   
+   ////Setters////
+   
+   //Sets the url 
+   public void setUrl (String url) {
+      this.url = url;
+   }
+   
+   //Sets the short URL
+   public void setShortUrl (String shortUrl) {
+      this.shortUrl = shortUrl;
+   }
+   
+   /////METHODS/////
+   
+   //Generates a random 6 character alphanumeric char array
+   //returns char array
+   public char[] generateSymbol () {
+      for(int i = 0; i < 6; i++) {
+         array[i] = alphaNumArray[r.nextInt(alphaNumArray.length)];   
+      }
+   return array;
+   }
+   
+   //returns a shortened URL using generateSymbol() method
+   public String shorten (String url) {
+      shortUrl = String.valueOf(generateSymbol());
+   return shortUrl;
+   }
+   
+   //restores the shortened URL to its original URL, and returns
+   //null if there is no such original URL
+   public String restore (String shortUrl) {
+      if (getShortUrl() != shortUrl) {
+         return null;
+      }
+   return url;
+   }
+   
+   public static void main(String[] args) {         
+   }
+   
+      
+      
+        
+   
+   
+   
+}
